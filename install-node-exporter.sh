b5369c7cd43ef6a0f7f6020ee69ecd5401dcbc96
Anjaneyulu U <108971082+uppuanjaneyulu@users.noreply.github.com>
2023-07-25 13:10:58 +0530

    Update install-node-exporter.sh
    update to latest version

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -1,9 +1,9 @@
 sudo useradd --no-create-home node_exporter
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
-sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
-rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xzf node_exporter-1.5.0.linux-amd64.tar.gz
+sudo cp node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.5.0.linux-amd64.tar.gz node_exporter-1.5.0.linux-amd64
 
 sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
 
