64c8122eb56998798cd7e84b67798afcf19884c6
Anjaneyulu U <108971082+uppuanjaneyulu@users.noreply.github.com>
2023-07-25 13:02:44 +0530

    Update install-grafana.sh
    Changed the version of grafana

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
-sudo dpkg -i grafana_7.3.4_amd64.deb
+wget https://dl.grafana.com/oss/release/grafana_9.5.6_amd64.deb
+sudo dpkg -i grafana_9.5.6_amd64.deb
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
